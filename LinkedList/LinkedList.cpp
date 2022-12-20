0999ab049316f7e894e5e5e304f30994a918d4f5
Sh1ro00 <david.pintar@student.um.si>
2022-12-20 00:02:11 -0500

    Implemented Delete Node Option

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -28,29 +28,66 @@ public:
 class LinkedList {
 private:
 	Node* head, * tail;
+	int size = 0;
 
 public:
 	LinkedList() { head = nullptr; tail = nullptr; }
 
+	//Adds node as the head (if not created) or end of the list
 	void Add(int data)
 	{
 		Node* temp = new Node(data);
-		
+
 		if (head == nullptr)
-		{
 			head = temp;
-			tail = temp;
-		}
 		else
-		{
 			tail->next = temp;
-			tail = temp;
-		}
+
+		//tail is the latest node added
+		tail = temp;
+
+		size++;
 	}
 
+	//Deletes node on given index (starts at 0)
+	void Delete(int index)
+	{
+		//Change head if index 0
+		if (index == 0)
+		{
+			Node* temp = head;
+			head = head->next;
+
+			//free up space
+			delete temp;
+			size--;
+			return;
+		}
+
+		//find node to delete
+		Node* prev, *temp;
+		prev = head;
+		temp = head;
+
+		for (int i = 0; i < index; i++)
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+		
+		//truncate previous with next node
+		prev->next = temp->next;
+		
+		//delete on index (frees up space)
+		delete temp;
+
+		size--;
+	}
+
+	//Prints data of all nodes
 	void Print()
 	{
-		if (head != nullptr)
+		if (size > 0)
 		{
 			Node* temp = head;
 			while (temp->next != nullptr)
@@ -63,6 +100,9 @@ public:
 		else
 			std::cout << "LinkedList is empty!" << std::endl;
 	}
+
+	//Size of linked list
+	int Size() { return size; }
 };
 
 
@@ -70,19 +110,25 @@ public:
 int main()
 {
 	//create linked list
-	LinkedList* linkedlist = new LinkedList();
+	LinkedList* linkedList = new LinkedList();
 
 	//add node to linked list
-	linkedlist->Add(5);
-	linkedlist->Add(7);
-	linkedlist->Add(99);
-	linkedlist->Add(24);
-	linkedlist->Add(69);
+	linkedList->Add(5);
+	linkedList->Add(77);
+	linkedList->Add(8);
+	linkedList->Add(24);
+	linkedList->Add(69);
 
 	//print linked list
-	linkedlist->Print();
+	std::cout << "Size: " << linkedList->Size() << std::endl;
+	linkedList->Print();
 
 	//delete node from linked list
+	linkedList->Delete(2);
+
+	//print linked list
+	std::cout << "Size: " << linkedList->Size() << std::endl;
+	linkedList->Print();
 
 	return 0;
 }
